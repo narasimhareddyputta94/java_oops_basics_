b915b5fa542ed00aac5c392a0e958c029533c4f7
narasimha reddy putta <150554709+narasimhareddyputta94@users.noreply.github.com>
2024-03-15 09:13:23 -0500

    created child class to check how accesmodifier

---
+++ src/main/java/Accessmodifierscheck/StudentChild.java
@@ -0,0 +1,14 @@
+package Accessmodifierscheck;
+
+import Accessmodifiers.Student;
+public class StudentChild extends Student{
+
+    public void dostuff() {
+        this.batchName = "B20";
+        this.courseName = "B.Tech";
+//        this.collegeName = "IIT";
+//        this.age = 20;
+//        this.changename("John");
+//        this.give_mock_test();
+    }
+}
