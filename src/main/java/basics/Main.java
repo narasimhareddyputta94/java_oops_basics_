1ba3064a6a1dcc45760cba598c79722218d84827
narasimha reddy putta <150554709+narasimhareddyputta94@users.noreply.github.com>
2024-03-14 21:09:52 -0500

    update Main.java

--- src/main/java/basics/Main.java
+++ src/main/java/basics/Main.java
@@ -13,5 +13,14 @@ public class Main {
         student1.changeCourse("M.Tech");
         student1.changeCollege("IIT Delhi");
         student1.display();
+
+        Student student2 = new Student();
+        student2.name = "Jane";
+        student2.age = 21;
+        student2.batchName = "B21";
+        student2.courseName = "M.Tech";
+        student2.collegeName = "IIT Delhi";
+        student2.display();
+
     }
 }
