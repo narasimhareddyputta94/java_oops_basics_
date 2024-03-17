bc14a2514e165bd6feda4d8f485fec06379f08ce
narasimha reddy putta <150554709+narasimhareddyputta94@users.noreply.github.com>
2024-03-17 15:59:40 -0500

    implemented till methodoverloading

---
+++ src/main/java/MethodOverloading/A.java
@@ -0,0 +1,30 @@
+package MethodOverloading;
+
+public class A {
+    int age;
+    String name;
+
+    public void display(int age, String name) {
+        System.out.println("Age: " + age + " Name: " + name);
+    }
+
+    public void display(String name, int age) {
+        System.out.println("Name: " + name + " Age: " + age);
+    }
+
+    public void display(int age, String name, String address) {
+        System.out.println("Age: " + age + " Name: " + name + " Address: " + address);
+    }
+
+    public void display(String name, int age, String address) {
+        System.out.println("Name: " + name + " Age: " + age + " Address: " + address);
+    }
+
+    public void display(String name, String address, int age) {
+        System.out.println("Name: " + name + " Address: " + address + " Age: " + age);
+    }
+
+    public void display(String address, String name, int age) {
+        System.out.println("Address: " + address + " Name: " + name + " Age: " + age);
+    }
+}
