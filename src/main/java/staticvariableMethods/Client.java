5db9ecfc00aad4ccc71864f84cdab930c620cc3f
narasimha reddy putta <150554709+narasimhareddyputta94@users.noreply.github.com>
2024-03-19 10:45:21 -0500

    learned bout static key and final keyword

---
+++ src/main/java/staticvariableMethods/Client.java
@@ -0,0 +1,9 @@
+package staticvariableMethods;
+
+public class Client {
+
+    public static void main(String[] args) {
+        System.out.println(Student.collegeName);
+        System.out.println(Student.name);
+    }
+}
---
+++ src/main/java/staticvariableMethods/Student.java
@@ -0,0 +1,14 @@
+package staticvariableMethods;
+
+public class Student {
+
+    static String name = "vinodh";
+
+    String address;
+
+    int rollNo;
+
+    static final String collegeName = "IIT";
+
+
+}
